b8214aee51596fe9de459f259cf360f5fca9fbc1
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-12-07 19:12:35 -0300

    Criação de instância do Observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
-import { from } from "rxjs";
+import { from, Observable } from "rxjs";
 
-let numbers = [1, 5, 10];
+let numbers = [1,5,10];
 let source = from(numbers);
 
-function component() {
-  source.subscribe((x) => console.log(x));
+
+let sourceInstance = new Observable(subscriber => {
+  for (let n of numbers) {
+    if (n > 5) {
+      subscriber.error('Aconteceu um erro esperado!');
+    }
+
+    subscriber.next(n);
+    subscriber.complete();
+  }
+  subscriber.complete();
+})
+
+
+const myObserver = {
+  next: (x: number) => { 
+    console.log(x) 
+  },
+  error: (e: Error) => console.log(e),
+  complete: () => console.log('Complete'),
 }
 
-component();
+function component() {
+  source.subscribe(myObserver)
+
+  sourceInstance.subscribe(myObserver)
+}
+
+component()
