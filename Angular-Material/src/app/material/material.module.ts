00c6f2d54875afadebf6307dc4151d612a2ff02d
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-12-12 19:02:47 -0300

    Adição de sidenav

--- Angular-Material/src/app/material/material.module.ts
+++ Angular-Material/src/app/material/material.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule],
 })
 export class MaterialModule {}
