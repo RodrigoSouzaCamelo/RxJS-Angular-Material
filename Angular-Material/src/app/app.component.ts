3e7f3f4ae34f0425a38b06c01216dacac67b03b9
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-12-13 19:33:33 -0300

    Refatoração do sidenav

--- Angular-Material/src/app/app.component.ts
+++ Angular-Material/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+import { Component } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
@@ -8,21 +7,16 @@ import { BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+  isSmallScreen: boolean = false;
+
+  get sideNavOpened() {
+    return !this.isSmallScreen;
+  }
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
   ngAfterContentInit() {
-    this.breakpointObserver.observe(['(max-width: 800px)']).subscribe({
-      next: (value) => {
-        if(value.matches) {
-          this.sideNav.mode = 'over';
-          this.sideNav.close();
-        } else {
-          this.sideNav.mode = 'side';
-          this.sideNav.open();
-        }
-      }
-    })
+    this.breakpointObserver.observe(['(max-width: 800px)'])
+    .subscribe((res) => this.isSmallScreen = res.matches);
   }
 }
