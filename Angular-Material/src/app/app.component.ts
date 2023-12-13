6fff65b4952a3ee4100bcf9862828e28d05c31c3
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-12-13 19:21:04 -0300

    Sidenav dinâmica utilizando rxjs

--- Angular-Material/src/app/app.component.ts
+++ Angular-Material/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-Material';
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngAfterContentInit() {
+    this.breakpointObserver.observe(['(max-width: 800px)']).subscribe({
+      next: (value) => {
+        if(value.matches) {
+          this.sideNav.mode = 'over';
+          this.sideNav.close();
+        } else {
+          this.sideNav.mode = 'side';
+          this.sideNav.open();
+        }
+      }
+    })
+  }
 }
