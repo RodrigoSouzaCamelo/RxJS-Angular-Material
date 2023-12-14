50c13a486a0516138896324bbd1a47f182c85ed3
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-12-14 19:55:56 -0300

    Toolbar fixo

--- Angular-Material/src/app/app.component.ts
+++ Angular-Material/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
+import { fromEvent, map } from 'rxjs';
+
+export const TEXT_LIMIT = 60;
+export const SHADOW_LIMIT = 100;
+export const SCROLL_CONTAINER = 'mat-sidenav-content';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  isSmallScreen: boolean = false;
+export class AppComponent implements OnInit {
+  public isSmallScreen: boolean = false;
+  public popText = false;
+  public applyShadow = false;
 
   get sideNavOpened() {
     return !this.isSmallScreen;
@@ -15,8 +22,24 @@ export class AppComponent {
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
+  ngOnInit(): void {
+    const content = document.getElementsByTagName(SCROLL_CONTAINER)[0];
+
+    console.log('content: ', content);
+
+    fromEvent(content, 'scroll')
+      .pipe(map(() => content.scrollTop))
+      .subscribe((value) => this.determineHeader(value));
+  }
+
+  determineHeader(value: number): void {
+    this.popText = value >= TEXT_LIMIT;
+    this.applyShadow = value >= SHADOW_LIMIT;
+  }
+
   ngAfterContentInit() {
-    this.breakpointObserver.observe(['(max-width: 800px)'])
-    .subscribe((res) => this.isSmallScreen = res.matches);
+    this.breakpointObserver
+      .observe(['(max-width: 800px)'])
+      .subscribe((res) => (this.isSmallScreen = res.matches));
   }
 }
