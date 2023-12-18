0cd97a67755267e3f2f62225ce130071ff88e0e5
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-12-18 19:51:47 -0300

    Adição de inputs do material

--- Angular-Material/src/app/material/material.module.ts
+++ Angular-Material/src/app/material/material.module.ts
@@ -3,9 +3,17 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatInputModule],
+  providers: [
+      {
+        provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+        useValue: { appearance: 'outline', floatLabel: 'always' }
+      }
+  ]
 })
 export class MaterialModule {}
