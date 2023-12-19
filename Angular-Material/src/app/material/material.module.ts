7f7a2d922e15c8d41201ebbbee3e328a48c3e20b
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-12-19 19:24:15 -0300

    Criando as rotas do projeto

--- Angular-Material/src/app/material/material.module.ts
+++ Angular-Material/src/app/material/material.module.ts
@@ -4,11 +4,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatInputModule],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatInputModule, MatListModule],
   providers: [
       {
         provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
