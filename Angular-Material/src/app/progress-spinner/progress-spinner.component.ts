7c008f26582f6176fda98c7f65f516d434b90d74
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-12-20 19:25:27 -0300

    loading indeterminate

--- Angular-Material/src/app/progress-spinner/progress-spinner.component.ts
+++ Angular-Material/src/app/progress-spinner/progress-spinner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, interval, map, takeWhile } from 'rxjs';
+import { ProgressBarMode } from '@angular/material/progress-bar';
+import { Observable, concat, interval, last, map, take, takeWhile, tap } from 'rxjs';
 
 @Component({
   selector: 'app-progress-spinner',
@@ -8,19 +9,30 @@ import { Observable, interval, map, takeWhile } from 'rxjs';
 })
 export class ProgressSpinnerComponent implements OnInit {
   public loadingPercent = 0;
+  public queryValue = 0;
+  public queryMode: ProgressBarMode = 'indeterminate';
 
   constructor() { }
 
   ngOnInit(): void {
-    this.loadingProgress(500)
-      .subscribe(x => this.loadingPercent = x)
+    this.loadingProgress(500, 95)
+      .subscribe(x => this.loadingPercent = x);
+
+      concat(
+        interval(5000)
+        .pipe(
+          take(1),
+          tap(_ => this.queryMode = 'determinate')
+        ),
+        this.loadingProgress(500, 100)
+      ).subscribe(x => this.queryValue = x);
   }
 
-  loadingProgress(speed: number) {
+  loadingProgress(speed: number, takeUntil: number) {
     return interval(speed)
       .pipe(
         map(i => i * 5),
-        takeWhile(i => i < 95)
+        takeWhile(i => i < takeUntil)
       );
   }
 
