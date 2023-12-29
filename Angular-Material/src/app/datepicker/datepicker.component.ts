46e166ee9e98653b171fca6750c8a3e1466b123b
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-12-29 20:37:38 -0300

    Formatação do datepicker

--- Angular-Material/src/app/datepicker/datepicker.component.ts
+++ Angular-Material/src/app/datepicker/datepicker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@angular/cdk/platform'
 
 @Component({
   selector: 'app-datepicker',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datepicker.component.scss']
 })
 export class DatepickerComponent implements OnInit {
+  public startDate: Date = new Date(Date.now());
+  public minDate: Date = new Date(this.startDate.getFullYear(), this.startDate.getMonth() - 1, this.startDate.getDate())
+  public maxDate: Date = new Date(this.startDate.getFullYear(), this.startDate.getMonth() + 1, this.startDate.getDate())
 
-  constructor() { }
+  constructor(private platform: Platform) { }
 
   ngOnInit(): void {
+  }
+
+  get isTouchDevice() {
+    return this.platform.ANDROID || this.platform.IOS;
   }
 
 }
