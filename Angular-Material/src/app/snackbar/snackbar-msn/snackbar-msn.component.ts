720f7f430baf945a1c1333f12d483e37e31b9a76
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-01-03 19:36:28 -0300

    Função de fechar o snackbar personalizável

--- Angular-Material/src/app/snackbar/snackbar-msn/snackbar-msn.component.ts
+++ Angular-Material/src/app/snackbar/snackbar-msn/snackbar-msn.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-snackbar-msn',
@@ -8,9 +8,14 @@ import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 })
 export class SnackbarMsnComponent implements OnInit {
 
-  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: string) { }
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: string,
+              public snackBarRef: MatSnackBarRef<SnackbarMsnComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  closeSnackBar() {
+    this.snackBarRef.dismiss();
   }
 
 }
